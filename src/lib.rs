5ae2d8f9873649afb9217c73e2a802f1a964c49c
Jonny <faane784@gmail.com>
2021-08-23 16:40:26 -0700

    Rename project

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,24 @@
 // Copyright 2021 Jonathan Manly.
 
-// This file is part of rustml.
+// This file is part of rml.
 
-// rustml is free software: you can redistribute it and/or modify
+// rml is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 
-// rustml is distributed in the hope that it will be useful,
+// rml is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU Lesser General Public License for more details.
 
 // You should have received a copy of the GNU Lesser General Public License
-// along with rustml.  If not, see <https://www.gnu.org/licenses/>.
+// along with rml.  If not, see <https://www.gnu.org/licenses/>.
 
 /*!
 Machine learning algorithms and tools.
 
-RustML contains machine learning algorithms and other tools to assist in solving
+rml contains machine learning algorithms and other tools to assist in solving
 machine learning problems using Rust.
 
 # Examples
